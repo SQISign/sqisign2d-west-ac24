28a61a51beb16189a3eb5c8fdbd1a7e57818cad6
Damien Robert <Damien.Olivier.Robert+git@gmail.com>
2024-02-23 14:09:45 +0100

    More bilinearity tests

--- src/ec/ref/ecx/test/biextension-test.c
+++ src/ec/ref/ecx/test/biextension-test.c
@@ -23,6 +23,7 @@ void dbl_2e(ec_point_t* R, uint64_t e, ec_point_t const* P, ec_point_t const* A2
     }
 }
 
+/*
 void cubical_2e(ec_point_t* R, uint64_t e, ec_point_t const* P, ec_point_t const* A24)
 {
     copy_point(R, P);
@@ -30,16 +31,16 @@ void cubical_2e(ec_point_t* R, uint64_t e, ec_point_t const* P, ec_point_t const
         cubicalDBL(R, R, A24);
     }
 }
+*/
 
 int biextension_test() 
 {
     ec_curve_t E0 = CURVE_E0; 
     uint64_t e = TORSION_PLUS_EVEN_POWER;
     // ibz_t two_pow, tmp;
-    fp2_t one, r1, rr1, r2, r3;
+    fp2_t one, r1, rr1, rrr1, r2, r3;
     ec_point_t P, Q, PmQ, A24, AC;
-    ec_point_t PQ, PP, QQ, PPQ, PQQ;
-    ec_point_t Pe, Qe, PmQe;
+    ec_point_t tmp, PQ, PP, QQ, PPQ, PQQ, PPP, QQQ, PPPQ, PQQQ;
 
     // ibz_init(&two_pow); ibz_init(&tmp);
     // ibz_pow(&two_pow,&ibz_const_two,length);
@@ -51,29 +52,26 @@ int biextension_test()
     copy_point(&PmQ, &BASIS_EVEN.PmQ);
 
     printf("Testing order of points\n");
-    dbl_2e(&Pe, e, &P, &A24);
-    dbl_2e(&Qe, e, &Q, &A24);
-    dbl_2e(&PmQe, e, &PmQ, &A24);
-    assert(ec_is_zero(&Pe));
-    assert(ec_is_zero(&Qe));
-    assert(ec_is_zero(&PmQe));
-
-    for (uint64_t i=0; i<e; i++) {
-      dbl_2e(&Pe, i, &P, &A24);
-      cubical_2e(&Qe, i, &P, &A24);
-      printf("i=%d: r=%d\n", i, is_point_equal(&Pe, &Qe));
-    }
-    cubical_2e(&Pe, e, &P, &A24);
-    assert(ec_is_zero(&Pe));
+    dbl_2e(&tmp, e, &P, &A24);
+    assert(ec_is_zero(&tmp));
+    dbl_2e(&tmp, e, &Q, &A24);
+    assert(ec_is_zero(&tmp));
+    dbl_2e(&tmp, e, &PmQ, &A24);
+    assert(ec_is_zero(&tmp));
 
     printf("Testing order of Weil pairing\n");
     xADD(&PQ, &P, &Q, &PmQ);
     weil(&r1, e, &P, &Q, &PQ, &A24);
+
     fp2_setone(&one);
     fp2_exp_2e(&r2, e, &r1);
     assert(fp2_is_equal(&r2, &one));
 
     printf("Bilinearity tests\n");
+    weil(&r2, e, &P, &Q, &PmQ, &A24);
+    fp2_inv(&r2);
+    assert(fp2_is_equal(&r1, &r2));
+
     xDBLv2(&PP, &P, &A24);
     xDBLv2(&QQ, &Q, &A24);
     xADD(&PPQ, &PQ, &P, &Q);
@@ -84,6 +82,16 @@ int biextension_test()
     assert(fp2_is_equal(&r2, &r3));
     fp2_sqr(&rr1, &r1);
     assert(fp2_is_equal(&rr1, &r2));
+
+    xADD(&PPP, &PP, &P, &P);
+    xADD(&QQQ, &QQ, &Q, &Q);
+    xADD(&PPPQ, &PPQ, &P, &PQ);
+    xADD(&PQQQ, &PQQ, &Q, &PQ);
+    weil(&r2, e, &PPP, &Q, &PPPQ, &A24);
+    weil(&r3, e, &P, &QQQ, &PQQQ, &A24);
+    assert(fp2_is_equal(&r2, &r3));
+    fp2_mul(&rrr1, &rr1, &r1);
+    assert(fp2_is_equal(&rrr1, &r2));
 
     return 1;
 }
