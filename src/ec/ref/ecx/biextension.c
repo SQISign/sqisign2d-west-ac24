4953921ffde176b5c8960ed24cc44d0acc991a2f
Damien Robert <Damien.Olivier.Robert+git@gmail.com>
2024-04-16 16:58:18 +0200

    Inline Weil pairings for basis dlog

--- src/ec/ref/ecx/biextension.c
+++ src/ec/ref/ecx/biextension.c
@@ -197,7 +197,7 @@ void to_cubical(ec_point_t* Q, ec_point_t* P) {
 }
 
 // Normalize the points and also store 1/x(P), 1/x(Q)
-void to_cubical_i(ec_point_t* Q, ec_point_t* P, fp2_t* ixP, fp2_t* ixQ) {
+void to_cubical_i(ec_point_t* P, ec_point_t* Q, fp2_t* ixP, fp2_t* ixQ) {
     /*
     //ec_normalize(A24);
     ec_normalize(P);
@@ -232,6 +232,7 @@ void to_cubical_c(ec_point_t* P, ec_point_t* A24, ec_point_t const* P_, ec_point
 
 // non reduced Tate pairing, PQ should be P+Q in (X:Z) coordinates
 // Assume the cubical points are normalised, and that we have 1/x(P)
+// The _n suffix means we assume the points are normalised
 void non_reduced_tate_n(fp2_t* r, uint64_t e, ec_point_t* P, ec_point_t* Q, ec_point_t* PQ, fp2_t const* ixP, ec_point_t* A24) {
     ec_point_t R;
     monodromy_i(&R, e, PQ, Q, P, ixP, A24);
@@ -242,7 +243,7 @@ void non_reduced_tate_n(fp2_t* r, uint64_t e, ec_point_t* P, ec_point_t* Q, ec_p
 void non_reduced_tate(fp2_t* r, uint64_t e, ec_point_t* P, ec_point_t* Q, ec_point_t* PQ, ec_point_t* A24) {
     // to_cubical(Q, P);
     fp2_t ixP, ixQ;
-    to_cubical_i(Q, P, &ixP, &ixQ); //TODO: ixQ not used
+    to_cubical_i(P, Q, &ixP, &ixQ); //TODO: ixQ not used
     non_reduced_tate_n(r, e, PQ, Q, P, &ixP, A24);
 }
 
@@ -265,9 +266,10 @@ void weil_n(fp2_t* r, uint64_t e, ec_point_t const* P, ec_point_t const* Q, ec_p
 // The code will crash (division by 0) if either P or Q is (0:1)
 void weil(fp2_t* r, uint64_t e, ec_point_t* P, ec_point_t* Q, ec_point_t* PQ, ec_point_t* A24) {
     fp2_t ixP, ixQ;
-    to_cubical_i(Q, P, &ixP, &ixQ);
+    to_cubical_i(P, Q, &ixP, &ixQ);
     weil_n(r, e, P, Q, PQ, &ixP, &ixQ, A24);
 }
+
 
 // recursive dlog function 
 bool fp2_dlog_2e_rec(digit_t *a, long len, fp2_t *pows_f, fp2_t *pows_g, long stacklen){
@@ -356,7 +358,7 @@ bool fp2_dlog_2e(digit_t* scal,const fp2_t *f,const fp2_t *g,int e){
 
 // compute the decomputation of basis on the basis PQ
 // note that basis might not actually be a basis (the order might me smaller than 2^e)
-void ec_dlog_2_weil(digit_t* scalarP1, digit_t* scalarQ1, digit_t* scalarP2, digit_t* scalarQ2, ec_basis_t* PQ, ec_basis_t *basis, ec_curve_t* curve,int e) {
+void ec_dlog_2_weil_old(digit_t* scalarP1, digit_t* scalarQ1, digit_t* scalarP2, digit_t* scalarQ2, ec_basis_t* PQ, ec_basis_t *basis, ec_curve_t* curve,int e) {
 
     assert(test_point_order_twof(&PQ->Q,curve,e));
 
@@ -406,6 +408,207 @@ void ec_dlog_2_weil(digit_t* scalarP1, digit_t* scalarQ1, digit_t* scalarP2, dig
     // e(P2,Q) = w0^scalarP2
     weil(&w,e,&basis->Q,&PQ->Q,&P2mQ,&A24);
     fp2_dlog_2e(scalarP2,&w,&w0,e);
+
+    #ifndef NDEBUG
+        ec_point_t test_comput;
+        ec_biscalar_mul(&test_comput,curve,scalarP1,scalarQ1,PQ);
+        assert(ec_is_equal(&test_comput,&basis->P));
+        ec_biscalar_mul(&test_comput,curve,scalarP2,scalarQ2,PQ);
+        assert(ec_is_equal(&test_comput,&basis->Q));
+    #endif
+
+}
+
+// Normalize a "basis" (P, Q), (P1, P2) and store their inverse
+void to_cubical_basis_i(
+    ec_point_t* P, ec_point_t* Q,
+    ec_point_t* P1, ec_point_t* P2,
+    fp2_t* ixP, fp2_t* ixQ,
+    fp2_t* ixP1, fp2_t* ixP2)
+{
+    fp2_t t[8];
+    fp2_copy(&t[0], &P->x);
+    fp2_copy(&t[1], &P->z);
+    fp2_copy(&t[2], &Q->x);
+    fp2_copy(&t[3], &Q->z);
+    fp2_copy(&t[4], &P1->x);
+    fp2_copy(&t[5], &P1->z);
+    fp2_copy(&t[6], &P2->x);
+    fp2_copy(&t[7], &P2->z);
+    fp2_batched_inv(t,8);
+    fp2_mul(ixP, &P->z, &t[0]);
+    fp2_mul(&P->x, &P->x, &t[1]);
+    fp2_setone(&P->z);
+    fp2_mul(ixQ, &Q->z, &t[2]);
+    fp2_mul(&Q->x, &Q->x, &t[3]);
+    fp2_setone(&Q->z);
+    fp2_mul(ixP1, &P1->z, &t[4]);
+    fp2_mul(&P1->x, &P1->x, &t[5]);
+    fp2_setone(&P1->z);
+    fp2_mul(ixP2, &P2->z, &t[6]);
+    fp2_mul(&P2->x, &P2->x, &t[7]);
+    fp2_setone(&P2->z);
+}
+
+void weil_dlog(digit_t* scalarP1, digit_t* scalarQ1,
+                digit_t* scalarP2, digit_t* scalarQ2,
+                uint64_t e,
+                ec_point_t* P, ec_point_t* Q, ec_point_t* P1, ec_point_t* P2,
+                ec_point_t* PQ,
+                ec_point_t* PP1, ec_point_t* PP2,
+                ec_point_t* P1Q, ec_point_t* P2Q,
+                ec_point_t* A24) {
+
+    fp2_t ixP, ixQ, ixP1, ixP2, w0, w;
+    ec_point_t nP, nQ, nP1, nP2, nPQ, PnQ, nPP1, PnP1, nPP2, PnP2, nP1Q, P1nQ, nP2Q, P2nQ, R0, R1;
+
+    to_cubical_basis_i(P, Q, P1, P2, &ixP, &ixQ, &ixP1, &ixP2);
+
+    copy_point(&nP, P);
+    copy_point(&nQ, Q);
+    copy_point(&nP1, P1);
+    copy_point(&nP2, P2);
+    copy_point(&nPQ, PQ);
+    copy_point(&PnQ, PQ);
+    copy_point(&nPP1, PP1);
+    copy_point(&nPP2, PP2);
+    copy_point(&PnP1, PP1);
+    copy_point(&PnP2, PP2);
+    copy_point(&nP1Q, P1Q);
+    copy_point(&nP2Q, P2Q);
+    copy_point(&P1nQ, P1Q);
+    copy_point(&P2nQ, P2Q);
+
+    for (uint64_t i=0; i<e-1; i++) {
+        cubicalADD(&nPQ, &nPQ, &nP, &ixQ);
+        cubicalADD(&nPP1, &nPP1, &nP, &ixP1);
+        cubicalADD(&nPP2, &nPP2, &nP, &ixP2);
+
+        cubicalADD(&PnQ, &PnQ, &nQ, &ixP);
+        cubicalADD(&P1nQ, &P1nQ, &nQ, &ixP1);
+        cubicalADD(&P2nQ, &P2nQ, &nQ, &ixP2);
+
+        cubicalADD(&PnP1, &PnP1, &nP1, &ixP);
+        cubicalADD(&nP1Q, &nP1Q, &nP1, &ixQ);
+
+        cubicalADD(&PnP2, &PnP2, &nP2, &ixP);
+        cubicalADD(&nP2Q, &nP2Q, &nP2, &ixQ);
+
+        cubicalDBL(&nP,  &nP,  A24);
+        cubicalDBL(&nQ,  &nQ,  A24);
+        cubicalDBL(&nP1, &nP1, A24);
+        cubicalDBL(&nP2, &nP2, A24);
+    }
+
+    // weil(&w0,e,&PQ->P,&PQ->Q,&PQ->PmQ,&A24);
+    translate(&nPQ, &nP);
+    translate(&nPP1, &nP);
+    translate(&nPP2, &nP);
+    translate(&PnQ, &nQ);
+    translate(&P1nQ, &nQ);
+    translate(&P2nQ, &nQ);
+    translate(&PnP1, &nP1);
+    translate(&nP1Q, &nP1);
+    translate(&PnP2, &nP2);
+    translate(&nP2Q, &nP2);
+
+    translate(&nP, &nP);
+    translate(&nQ, &nQ);
+    translate(&nP1, &nP1);
+    translate(&nP2, &nP2);
+
+    // TODO: we could batch these 5 inversions
+
+    // computation of the reference weil pairing 
+    // weil(&w0,e,&PQ->P,&PQ->Q,&PQ->PmQ,&A24);
+    point_ratio(&R0, &nPQ, &nP, Q);
+    point_ratio(&R1, &PnQ, &nQ, P);
+    fp2_mul(&w0, &R0.x, &R1.z);
+    fp2_inv(&w0);
+    fp2_mul(&w0, &w0, &R0.z);
+    fp2_mul(&w0, &w0, &R1.x);
+
+    // e(P,P1) = w0^scalarQ1
+    // weil(&w,e,&PQ->P,&basis->P,&PmP1,&A24);
+    point_ratio(&R0, &nPP1, &nP, P1);
+    point_ratio(&R1, &PnP1, &nP1, P);
+    fp2_mul(&w, &R0.x, &R1.z);
+    fp2_inv(&w);
+    fp2_mul(&w, &w, &R0.z);
+    fp2_mul(&w, &w, &R1.x);
+    fp2_dlog_2e(scalarQ1,&w,&w0,e);
+
+    // e(P1,Q) = w0^scalarP1
+    // weil(&w,e,&basis->P,&PQ->Q,&P1mQ,&A24);
+    point_ratio(&R0, &nP1Q, &nP1, Q);
+    point_ratio(&R1, &P1nQ, &nQ, P1);
+    fp2_mul(&w, &R0.x, &R1.z);
+    fp2_inv(&w);
+    fp2_mul(&w, &w, &R0.z);
+    fp2_mul(&w, &w, &R1.x);
+    fp2_dlog_2e(scalarP1,&w,&w0,e);
+
+    // e(P,P2) = w0^scalarQ2
+    // weil(&w,e,&PQ->P,&basis->Q,&PmP2,&A24);
+    point_ratio(&R0, &nPP2, &nP, P2);
+    point_ratio(&R1, &PnP2, &nP2, P);
+    fp2_mul(&w, &R0.x, &R1.z);
+    fp2_inv(&w);
+    fp2_mul(&w, &w, &R0.z);
+    fp2_mul(&w, &w, &R1.x);
+    fp2_dlog_2e(scalarQ2,&w,&w0,e);
+
+    // e(P2,Q) = w0^scalarP2
+    //weil(&w,e,&basis->Q,&PQ->Q,&P2mQ,&A24);
+    point_ratio(&R0, &nP2Q, &nP2, Q);
+    point_ratio(&R1, &P2nQ, &nQ, P2);
+    fp2_mul(&w, &R0.x, &R1.z);
+    fp2_inv(&w);
+    fp2_mul(&w, &w, &R0.z);
+    fp2_mul(&w, &w, &R1.x);
+    fp2_dlog_2e(scalarP2,&w,&w0,e);
+}
+
+// Like ec_dlog_2_weil_old but inline all weil pairing computation to factor
+// the same arithmetic operations
+void ec_dlog_2_weil(digit_t* scalarP1, digit_t* scalarQ1, digit_t* scalarP2, digit_t* scalarQ2, ec_basis_t* PQ, ec_basis_t *basis, ec_curve_t* curve,int e) {
+
+    assert(test_point_order_twof(&PQ->Q,curve,e));
+
+
+    fp2_t w0,w;
+    ec_point_t AC,A24;
+    ec_point_t PmP1,P1mQ,PmP2,P2mQ;
+    jac_point_t xyP,xyQ,xyP1,xyP2,temp;
+
+    // we start by computing the different weil pairings
+
+    // precomputing the correct curve data
+    fp2_copy(&AC.x,&curve->A);
+    fp2_copy(&AC.z,&curve->C);
+    A24_from_AC(&A24, &AC);
+
+    // lifting the two basis points
+    lift_basis(&xyP,&xyQ,PQ,curve);
+    lift_basis(&xyP1,&xyP2,basis,curve);
+
+    // computation of the differences 
+    jac_neg(&temp,&xyP1);
+    ADD(&temp,&temp,&xyP,curve);
+    jac_to_xz(&PmP1,&temp);
+    jac_neg(&temp,&xyP2);
+    ADD(&temp,&temp,&xyP,curve);
+    jac_to_xz(&PmP2,&temp);
+    jac_neg(&temp,&xyQ);
+    ADD(&temp,&temp,&xyP1,curve);
+    jac_to_xz(&P1mQ,&temp);
+    jac_neg(&temp,&xyQ);
+    ADD(&temp,&temp,&xyP2,curve);
+    jac_to_xz(&P2mQ,&temp);
+
+    weil_dlog(scalarP1, scalarQ1, scalarP2, scalarQ2, e,
+              &PQ->P, &PQ->Q, &basis->P, &basis->Q,
+              &PQ->PmQ,&PmP1, &PmP2, &P1mQ, &P2mQ, &A24);
 
     #ifndef NDEBUG
         ec_point_t test_comput;
